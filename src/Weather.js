c4d5f33a25a029bd57c8e61cfd8ac4b456dca2cc
lwalzel <lisa.walzel@hotmail.com>
2024-06-23 18:46:45 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -12,6 +12,7 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -24,13 +25,19 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"
-          />{" "}
-          6°
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            />{" "}
+            <div className="float-left">
+              <span className="temperature">6</span>{" "}
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>

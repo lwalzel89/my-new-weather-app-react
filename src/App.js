949dd42fc8455a77b59bf292af63ae88b26d0089
lwalzel <lisa.walzel@hotmail.com>
2024-06-23 18:06:11 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lisa Walzel and is{" "}
-        <a
-          href="https://github.com/lwalzel89/my-new-weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Lisa Walzel and is{" "}
+          <a
+            href="https://github.com/lwalzel89/my-new-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

37f35921f6f9ac2d858a830a647d4cb0bd3ca611
lwalzel <lisa.walzel@hotmail.com>
2024-07-01 00:08:10 +0200

    Added the animated icons

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: response.data.weather[0].icon,
+      icon: response.data.weather[0].icon,
       city: response.data.name,
     });
   }
--- src/WeatherInformation.js
+++ src/WeatherInformation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,9 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />{" "}
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
             <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}
@@ -30,7 +29,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}</li>
+            <li>Wind: {props.data.wind}km/h</li>
           </ul>
         </div>
       </div>

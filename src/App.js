4c016c289a1cebb96a35486e961842d00968601f
lwalzel <lisa.walzel@hotmail.com>
2024-07-01 00:34:24 +0200

    Added Netlify-link

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ export default function App() {
           >
             open-sourced on GitHub.
           </a>
+          and{" "}
+          <a
+            href="https://inspiring-kitten-637218.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          .
         </footer>
       </div>
     </div>

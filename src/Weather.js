cd27b1bb1a972e828fff090b32a0a7070711d24f
lwalzel <lisa.walzel@hotmail.com>
2024-07-07 14:51:33 +0200

    Created layout of the forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInformation from "./WeatherInformation";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -55,6 +56,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInformation data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
